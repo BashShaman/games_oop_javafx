57959dd862f4cc6a8af4727a0e934fa66a15ec3d
BashW <BashWhiteShaman@yandex.com>
2022-06-05 14:59:31 +0700

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -26,11 +26,10 @@ public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = false;
         for (int i = 0; i < board.length; i++) {
-            if (board[i][i] == 1) {
-                if (isFilledColumn(board, i) || isFilledRow(board, i)) {
-                    rsl = true;
-                    break;
-                }
+            if ((board[i][i] == 1)
+                    && (isFilledColumn(board, i) || isFilledRow(board, i))) {
+                rsl = true;
+                break;
             }
         }
         return rsl;
